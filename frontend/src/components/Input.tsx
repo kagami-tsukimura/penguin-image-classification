19513772f4aaf5277dca96c5245561237ec1ef67
Kagami <shimura.kyohei7617@gmail.com>
2023-10-01 00:52:13 +0900

    update: tooltip

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,31 +1,52 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Input = () => {
-  const [imageMessage, setImageMessage] =
-    useState('分類したい画像を選択してください');
-  const [profileImage, setProfileImage] = useState('default-profile.png');
+  const [profileImage, setProfileImage] = useState<string>(
+    'default-profile.png'
+  );
+  const [isTooltip, setIsTooltip] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null!);
+
+  const onProfileButtonClick = () => {
+    // useRef<HTMLInputElement>のcurrent要素を呼び出し、ファイル選択画面を表示
+    inputRef.current.click();
+  };
+
   const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
 
     // React.ChangeEvent<HTMLInputElement>よりファイルを取得
     const fileObject = e.target.files[0];
     // オブジェクトURLを生成し、useState()を更新
-    setImageMessage('');
     setProfileImage(window.URL.createObjectURL(fileObject));
   };
 
   return (
     <div className='items-center mt-8'>
-      <p className='text-left mb-8'>{imageMessage}</p>
       <div className='flex items-center justify-center'>
-        <img src={profileImage} className='h-32 w-32 rounded-full' />
+        <img src={profileImage} className='object-contain h-32 w-32' />
       </div>
       <input
+        hidden
+        ref={inputRef}
         type='file'
         accept='image/*'
         onChange={onFileInputChange}
-        className='items-center pt-8'
       />
+      {/* ファイル選択用のボタンを用意 */}
+      <button
+        className='bg-violet-700 hover:bg-violet-900 text-white px-4 py-2 rounded-md mt-4 '
+        onClick={onProfileButtonClick}
+        onMouseMove={() => setIsTooltip(true)}
+        onMouseLeave={() => setIsTooltip(false)}
+      >
+        分類する画像を選択
+      </button>
+      {isTooltip && (
+        <div className='absolute mt-4 text-gray-400 rounded text-xs'>
+          HINT: お気に入りのペンギンを選択しよう
+        </div>
+      )}
     </div>
   );
 };
