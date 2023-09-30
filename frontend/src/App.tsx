41099f36e9b06685046d43dbf51c227431c07158
Kagami <shimura.kyohei7617@gmail.com>
2023-09-30 22:36:02 +0900

    style: body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Input from './components/Input';
+import Output from './components/Output';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <div className='flex items-center justify-between'>
+        <Input />
+        <Output />
+      </div>
+      <Footer />
     </>
   );
 };
