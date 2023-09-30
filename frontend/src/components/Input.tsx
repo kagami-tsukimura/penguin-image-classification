c4e3d45d4049c6bdfc76a91a5bfd87fdfd667ca8
Kagami <shimura.kyohei7617@gmail.com>
2023-10-01 00:19:36 +0900

    update: input image

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
 const Input = () => {
-  return <div>Input</div>;
+  const [imageMessage, setImageMessage] =
+    useState('分類したい画像を選択してください');
+  const [profileImage, setProfileImage] = useState('default-profile.png');
+  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    // React.ChangeEvent<HTMLInputElement>よりファイルを取得
+    const fileObject = e.target.files[0];
+    // オブジェクトURLを生成し、useState()を更新
+    setImageMessage('');
+    setProfileImage(window.URL.createObjectURL(fileObject));
+  };
+
+  return (
+    <div className='items-center mt-8'>
+      <p className='text-left mb-8'>{imageMessage}</p>
+      <div className='flex items-center justify-center'>
+        <img src={profileImage} className='h-32 w-32 rounded-full' />
+      </div>
+      <input
+        type='file'
+        accept='image/*'
+        onChange={onFileInputChange}
+        className='items-center pt-8'
+      />
+    </div>
+  );
 };
 
 export default Input;
