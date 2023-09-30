6137c7b64fe3e938d46b2d4d42ae3e972985e756
Kagami <shimura.kyohei7617@gmail.com>
2023-09-30 12:50:11 +0900

    style: frontend template

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# React + TypeScript + Vite + Tailwindcss Study
+
+## Read
+
+1. [The Types in TypeScript](docs/fundamental/types.md)
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='p-2 text-teal-200'>ペンギンの分類</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
