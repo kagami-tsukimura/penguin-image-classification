c6b557b56e9834a6cb7d44f0904baf7ca974bc91
Kagami <shimura.kyohei7617@gmail.com>
2023-10-01 11:53:33 +0900

    CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORSミドルウェアを有効にする
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # すべてのオリジンからのリクエストを許可（開発用途）
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # 起動確認
