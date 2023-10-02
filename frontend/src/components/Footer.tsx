cdb75b046e675055dfa67d261577dff2b752a95f
Kagami <shimura.kyohei7617@gmail.com>
2023-10-02 21:00:22 +0900

    refactor: icon css

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -30,10 +30,7 @@ const Footer: React.FC = () => {
 
         {/* <!-- Social Brands --> */}
         <div className='mt-3 space-x-2'>
-          <a
-            className='inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800'
-            href='mailto:kagami.penguin.t@gmail.com'
-          >
+          <a className='icon' href='mailto:kagami.penguin.t@gmail.com'>
             <svg
               className='w-3.5 h-3.5'
               xmlns='http://www.w3.org/2000/svg'
@@ -45,10 +42,7 @@ const Footer: React.FC = () => {
               <path d='M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z' />
             </svg>
           </a>
-          <a
-            className='inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800'
-            href='https://twitter.com/tukimurakagami'
-          >
+          <a className='icon' href='https://twitter.com/tukimurakagami'>
             <svg
               className='w-3.5 h-3.5'
               xmlns='http://www.w3.org/2000/svg'
@@ -60,10 +54,7 @@ const Footer: React.FC = () => {
               <path d='M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z' />
             </svg>
           </a>
-          <a
-            className='inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800'
-            href='https://github.com/kagami-tsukimura'
-          >
+          <a className='icon' href='https://github.com/kagami-tsukimura'>
             <svg
               className='w-3.5 h-3.5'
               xmlns='http://www.w3.org/2000/svg'
