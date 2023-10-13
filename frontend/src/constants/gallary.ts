13ee935272d4e1b77508e070e6fc3f978701227a
Kagami <shimura.kyohei7617@gmail.com>
2023-10-13 20:50:47 +0900

    update: add sample image

--- frontend/src/constants/gallary.ts
+++ frontend/src/constants/gallary.ts
@@ -20,4 +20,36 @@ export const sample: Samples[] = [
     id: 4,
     image: 'https://source.unsplash.com/TtQm-1ra4IU',
   },
+  {
+    id: 5,
+    image: 'https://source.unsplash.com/0xOUy_nbsDY',
+  },
+  {
+    id: 6,
+    image: 'https://source.unsplash.com/pjt4AzvfTh0',
+  },
+  {
+    id: 7,
+    image: 'https://source.unsplash.com/XJV36au6YG0',
+  },
+  {
+    id: 8,
+    image: 'https://source.unsplash.com/ZZyK8GjFJ4Q',
+  },
+  {
+    id: 9,
+    image: 'https://source.unsplash.com/9EpHbo02VXY',
+  },
+  {
+    id: 10,
+    image: 'https://source.unsplash.com/D20_h2fPlk0',
+  },
+  {
+    id: 11,
+    image: 'https://source.unsplash.com/22EqbN0yI-4',
+  },
+  {
+    id: 12,
+    image: 'https://source.unsplash.com/9knYol3f2qY',
+  },
 ];
