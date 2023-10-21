e0c65f3a87d26c7bbbe773a249dd8f6aedb7c3f9
Kagami <shimura.kyohei7617@gmail.com>
2023-10-21 11:27:11 +0900

    feat: darkmode to smartphone

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -19,8 +19,10 @@ const Header: React.FC = () => {
             ペンギンの分類
             <GiPenguin className='transform scale-x-[-1]' />
           </a>
-          <div className='sm:hidden'>
-            <button
+          <div className='flex-initial absolute right-2 gap-x-2 border-l pl-2 border-gray-300 dark:border-gray-700 sm:hidden'>
+            <DarkModeButton />
+
+            {/* <button
               type='button'
               className='hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800'
               data-hs-collapse='#navbar-collapse-with-animation'
@@ -48,7 +50,7 @@ const Header: React.FC = () => {
               >
                 <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z' />
               </svg>
-            </button>
+            </button> */}
           </div>
         </div>
         <div
